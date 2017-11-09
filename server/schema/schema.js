076d2ea43f68a7115d64e45bc0046543abbdff71
hao <klaushaozhang@gmial.com>
2017-11-09 22:40:06 +1300

    add to graphql server

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -43,7 +43,36 @@ type Creator {
   comics: [Comic]
 }
 
+# a user
+type User{
+  email:String
+}
+
+# login payload
+input UserCredentialInput{
+  email:String!
+  password:String!
+}
+
+# indicate if the login is successful
+type LoginResult{
+  success:Boolean
+  email:String
+}
+
+# indicate if the user already exists
+type EmailAvalibilityCheckResult{
+  isUserExist:Boolean
+}
+
+
 type Query {
+  # login a user with credential
+  login(credential:UserCredentialInput!):LoginResult
+  
+  # check if an user exists
+  isEmailRegistered(email:String!):EmailAvalibilityCheckResult
+
   comic(id: Int!): Comic
   comics(
     start: Int = 0, 
@@ -69,4 +98,10 @@ type Query {
     nameStartsWith: String
   ): [Creator]
 }
+
+type Mutation{
+  # create a user with given credential. error when create failed.
+  createUser(credential:UserCredentialInput!):User
+}
+
 `
